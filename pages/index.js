c7de8b0250da9a83624a9891bc28b09aa8f27933
unknown <mascarenhas.alexandre@gmail.com>
2021-06-17 00:18:08 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,14 @@ import { useState } from 'react';
 function Home() {
    return (
        <div>
-        <h1>Home</h1>
-        <Contador />
+        <h1>Junior e mãe do hastad</h1>
+        <img 
+      src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.18169-9/15193489_1183970361684491_4162784252824567745_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeET4PtGPE5ucEnl_CHoFbCxip_waSySpGGKn_BpLJKkYbTyQaH3X86Sn7mYysh9AAGQlrSB2a6h75RLAStuAlNH&_nc_ohc=R-6D3k4Vl7EAX96FG-T&_nc_ht=scontent.fcpq4-1.fna&oh=ee292d9016a28517daac5d49d98dbeb1&oe=60CF8F9A"
+      alt="new"
+      />
        </div>
        
    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-
-    )
 }
 
 export default Home
